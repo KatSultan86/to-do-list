c558a126e0ea7a6cd10f42f8cc2bbcf3a13a49c2
KatSultan86 <shelest233@gmail.com>
2025-02-09 21:42:46 -0500

    code added

--- src/components/SubmissionForm.jsx
+++ src/components/SubmissionForm.jsx
@@ -3,33 +3,28 @@ import { FaPlus } from "react-icons/fa";
 
 function SubmissionForm() {
   return (
-    <form className="container col-11 col-lg-5 mt-5">
-      <h2 className="text-center">Todo List</h2>
+    <div className="subform">
+      <form className="container col-11 col-lg-5 mt-5">
+        <h2 className="text-center fw-bold">Todo List</h2>
 
-      <div className="card rounded-1 m-1 mt-4">
-        <span className="input-group-text border-0">
-          <div className="mar-Input mb-1">
-            <BsCheckCircleFill size={"1.3rem"} />
-          </div>
-          <input
-            type="text"
-            class="form-control border-0"
-            id="exampleInput"
-            placeholder="Create a new task..."
-          ></input>
-          <div className="mb-1">
-            <FaPlus />
-          </div>
-        </span>
-      </div>
-    </form>
+        <div className="card rounded-1 mt-4">
+          <span className="input-group-text border-0">
+            <div className="mar-Input mb-1">
+              <BsCheckCircleFill size={"1.3rem"} />
+            </div>
+            <input
+              type="text"
+              class="form-control border-0"
+              id="exampleInput"
+              placeholder="Create a new task..."
+            ></input>
+            <div className="mb-1">
+              <FaPlus />
+            </div>
+          </span>
+        </div>
+      </form>
+    </div>
   );
 }
 export default SubmissionForm;
-
-{
-  /* <div class="input-group">
-<span class="input-group-text"><i class="fas fa-search"></i></span>
-<input type="text" class="form-control" id="exampleInput" placeholder="Search...">
-</div> */
-}
--- src/components/ToDoListcard.jsx
+++ src/components/ToDoListcard.jsx
@@ -4,7 +4,35 @@ import { FaTrashCan } from "react-icons/fa6";
 function ToDoListcard() {
   return (
     <div className="container  col-11 col-lg-5">
-      <div className="card rounded-1 m-1 ">
+      <div className="card rounded-1  ">
+        <div className="card-bottom-line">
+          <ul className="list-group border-0">
+            <li className="list-group-item border-0 d-flex justify-content-between">
+              <div className="mar-checkmarck mb-1">
+                <BsCheckCircleFill size={"1.3rem"} />
+              </div>
+              <span className="me-auto ms-2 ps-1">Morning workout</span>
+              <div className="mar-trashcan">
+                <FaTrashCan />
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        <div className="card-bottom-line">
+          <ul className="list-group border-0">
+            <li className="list-group-item border-0 d-flex justify-content-between">
+              <div className="mar-checkmarck mb-1">
+                <BsCheckCircleFill size={"1.3rem"} />
+              </div>
+              <span className="me-auto ms-2 ps-1">Morning workout</span>
+              <div className="mar-trashcan">
+                <FaTrashCan />
+              </div>
+            </li>
+          </ul>
+        </div>
+
         <div className="card-bottom-line">
           <ul className="list-group border-0">
             <li className="list-group-item border-0 d-flex justify-content-between">
