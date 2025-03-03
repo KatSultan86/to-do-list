57b91ba35e7be8147f85c6b0014dd1db6ba6f00f
KatSultan86 <shelest233@gmail.com>
2025-03-02 21:16:19 -0500

    aadded code

--- src/components/ToDoListcard.jsx
+++ src/components/ToDoListcard.jsx
@@ -8,24 +8,24 @@ function ToDoListcard({ item, removeItem, completedItem, itemCount }) {
   const [filterType, setFilterType] = useState("all");
 
   function handleFiltering() {
+    let filteredItems = [];
+
     if (filterType === "completed") {
-      if ([item.filter((currItem) => currItem.isCompleted)].length > 0) {
-        return item.filter((currItem) => currItem.isCompleted);
-      } else if ([item.filter((currItem) => currItem.isCompleted)].length < 1) {
-        return <BaseHTML itemCount={itemCount} />;
-      }
+      filteredItems = item.filter((currItem) => currItem.isCompleted);
     } else if (filterType === "active") {
-      if ([item.filter((currItem) => !currItem.isCompleted)].length > 0) {
-        return item.filter((currItem) => !currItem.isCompleted);
-      } else if (
-        [item.filter((currItem) => !currItem.isCompleted)].length < 1
-      ) {
-        return <BaseHTML itemCount={itemCount} />;
-      }
+      filteredItems = item.filter((currItem) => !currItem.isCompleted);
     } else if (filterType === "clearCompleted") {
-      return item.filter((compItem) => !compItem.isCompleted);
+      filteredItems = item.filter((compItem) => !compItem.isCompleted);
+    } else {
+      filteredItems = item;
     }
-    return item;
+
+    if (filteredItems.length === 0) {
+      filteredItems = [{ component: <BaseHTML itemCount={itemCount} /> }];
+      return filteredItems;
+    }
+
+    return filteredItems;
   }
 
   return item.length < 1 ? (
@@ -34,41 +34,46 @@ function ToDoListcard({ item, removeItem, completedItem, itemCount }) {
     <div className="container col-11 col-lg-5">
       <div className=" custom-color card rounded-1  ">
         <ul className="list-group border-0 ">
-          {handleFiltering().map((items) => (
-            <div key={items.id} className="card-bottom-line ">
-              <li className=" custom-color list-group-item border-0 d-flex justify-content-between bg-card-form">
-                <div
-                  className=" mar-checkmarck mb-1 opacity-25"
-                  onClick={() => {
-                    completedItem(items);
-
-                    itemCount();
-                  }}
-                >
-                  {items.isCompleted ? (
-                    <BsCheckCircleFill size={"1.3rem"} />
-                  ) : (
-                    <IoMdRadioButtonOff size={"1.7rem"} />
-                  )}
-                </div>
-                <span
-                  className={`${
-                    items.isCompleted ? "text-decoration-line-through" : ""
-                  } me-auto ms-2 ps-1 opacity-75`}
-                >
-                  {items.item}
-                </span>
-                <button
-                  onClick={() => {
-                    removeItem(items.id);
-                  }}
-                  className="mar-trashcan btn opacity-25"
-                >
-                  <FaTrashCan />
-                </button>
-              </li>
-            </div>
-          ))}
+          {handleFiltering().map((items) =>
+            items.component ? (
+              <div className="text-center font-size p-1 opacity-50">
+                No Tasks Available
+                <div className="card-bottom-line-up "></div>
+              </div>
+            ) : (
+              <div key={items.id} className="card-bottom-line ">
+                <li className=" custom-color list-group-item border-0 d-flex justify-content-between bg-card-form">
+                  <div
+                    className=" mar-checkmarck mb-1 opacity-25"
+                    onClick={() => {
+                      completedItem(items);
+                    }}
+                  >
+                    {items.isCompleted ? (
+                      <BsCheckCircleFill size={"1.3rem"} />
+                    ) : (
+                      <IoMdRadioButtonOff size={"1.7rem"} />
+                    )}
+                  </div>
+                  <span
+                    className={`${
+                      items.isCompleted ? "text-decoration-line-through" : ""
+                    } me-auto ms-2 ps-1 opacity-75`}
+                  >
+                    {items.item}
+                  </span>
+                  <button
+                    onClick={() => {
+                      removeItem(items.id);
+                    }}
+                    className="mar-trashcan btn opacity-25"
+                  >
+                    <FaTrashCan />
+                  </button>
+                </li>
+              </div>
+            )
+          )}
         </ul>
 
         <div className="font-size">
